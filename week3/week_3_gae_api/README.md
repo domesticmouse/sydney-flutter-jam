7f51ca2e89941a43bfb3913effdea94f50046e82
Brett Morgan <brettmorgan@google.com>
2018-10-23 13:16:23 +1100

    Working Recipe API

--- week3/week_3_gae_api/README.md
+++ week3/week_3_gae_api/README.md
@@ -41,12 +41,115 @@ To view your application in the web browser run:
 
 ### List Recipes
 
-```
-GET /recipes
+```bash
+$ curl https://flutter-recipe-api.appspot.com/recipes/
+[
+   {
+      "id":1,
+      "name":"Macaroni Cheese",
+      "short_description":"An all too cheesy home cooked favourite",
+      "image_id":"fxm3oIlRapk"
+   },
+   {
+      "id":2,
+      "name":"Spaghetti Bolognese",
+      "short_description":"A heart warming classic, perfect for a cold winter's night",
+      "image_id":"JDEEsQKEm7I"
+   }
+]
 ```
 
 ### Get Recipe Details
 
-```
-GET /recipe/1
+```bash
+$ curl https://flutter-recipe-api.appspot.com/recipes/2
+{
+   "id":2,
+   "name":"Spaghetti Bolognese",
+   "short_description":"A heart warming classic, perfect for a cold winter's night",
+   "image_id":"JDEEsQKEm7I",
+   "ingredients":[
+      {
+         "quantity":2,
+         "measure":"tbs",
+         "ingredient":"Olive oil"
+      },
+      {
+         "quantity":2,
+         "measure":"",
+         "ingredient":"Brown onion"
+      },
+      {
+         "quantity":4,
+         "measure":"cloves",
+         "ingredient":"Garlic"
+      },
+      {
+         "quantity":1,
+         "measure":"kg",
+         "ingredient":"Beef mince"
+      },
+      {
+         "quantity":90,
+         "measure":"g",
+         "ingredient":"Tomato paste"
+      },
+      {
+         "quantity":500,
+         "measure":"ml",
+         "ingredient":"Beef stock"
+      },
+      {
+         "quantity":2,
+         "measure":"cans",
+         "ingredient":"Chopped tomatoes"
+      },
+      {
+         "quantity":1,
+         "measure":"",
+         "ingredient":"Bay leaf"
+      },
+      {
+         "quantity":400,
+         "measure":"g",
+         "ingredient":"Spaghetti"
+      },
+      {
+         "quantity":50,
+         "measure":"g",
+         "ingredient":"Shaved Parmesan"
+      }
+   ],
+   "preparation":[
+      {
+         "step":"Roughly chop brown onions"
+      },
+      {
+         "step":"Skin and finely mince garlic"
+      }
+   ],
+   "cooking":[
+      {
+         "step":"Heat oil in a pan, once shimmering add onion and stir over medium heat until softened"
+      },
+      {
+         "step":"Add garlic and cook for 20 seconds, until fragrant"
+      },
+      {
+         "step":"Add Beef mince and cook for 5 minutes, breaking apart and turning until browned all over"
+      },
+      {
+         "step":"Add tomatoes, tomato paste, beef stock and season with salt and pepper. Bring to a boild, then simmer for an hour until reduced"
+      },
+      {
+         "step":"Cook pasta in salted water until al dente"
+      },
+      {
+         "step":"Reserve a couple of cups of the pasta water,then drain the pasta"
+      },
+      {
+         "step":"In a frypan combine the pasta, a healthy serving of sauce, and some pasta water. Agitate over heat until combined"
+      }
+   ]
+}
 ```
--- week3/week_3_gae_api/helloworld.go
+++ week3/week_3_gae_api/helloworld.go
@@ -10,10 +10,111 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
+
+// Ingredient is an ingredient used in a recipe
+type Ingredient struct {
+	Quantity   int    `json:"quantity"`
+	Measure    string `json:"measure"`
+	Ingredient string `json:"ingredient"`
+}
+
+// Step is a step in the preparation and/or cooking of a recipe
+type Step struct {
+	Description string `json:"step"`
+}
+
+// Recipe is a description of how to cook a dish
+type Recipe struct {
+	ID               int          `json:"id"`
+	Name             string       `json:"name"`
+	ShortDescription string       `json:"short_description"`
+	ImageID          string       `json:"image_id"`
+	Ingredient       []Ingredient `json:"ingredients"`
+	Preparation      []Step       `json:"preparation"`
+	Cooking          []Step       `json:"cooking"`
+}
+
+// RecipeHeader is the highlights of a recipe for a summary view.
+type RecipeHeader struct {
+	ID               int    `json:"id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageID          string `json:"image_id"`
+}
+
+// Recipes is a list of recipes headers, for a summary view.
+type Recipes struct {
+	Recipes []RecipeHeader `json:"recipes"`
+}
+
+var recipes = []Recipe{
+	{
+		1,
+		"Macaroni Cheese",
+		"An all too cheesy home cooked favourite",
+		"fxm3oIlRapk",
+		[]Ingredient{
+			{500, "g", "Macaroni"},
+			{50, "g", "Butter"},
+			{2, "tbs", "Flour"},
+			{500, "ml", "Milk"},
+			{300, "g", "Tasty cheese"},
+			{50, "g", "Panko breadcrumbs"},
+			{125, "g", "Bacon"},
+		},
+		[]Step{
+			{"Grate tasty cheese"},
+			{"Dice bacon"},
+			{"Preheat oven to 200 degrees C"},
+			{"Grease oven proof dish with butter"},
+		},
+		[]Step{
+			{"Cook macaroni until almost al dente"},
+			{"Warm milk in microwave or small pan"},
+			{"Melt butter in a pan"},
+			{"Work flour into melted butter and cook for a minute over medium heat"},
+			{"Slowly work warmed milk into butter & flour mixture, stiring constantly until smooth. Bring to the boil, then simmer for a couple of minutes."},
+			{"Remove the mixture from the heat and fold in the grated cheese, bacon, and cooked macaroni"},
+			{"Put mixture in oven proof dish, cover with Panko breadcrumbs, season with salf and pepper, and cook for 15 minutes"},
+		},
+	},
+	{
+		2,
+		"Spaghetti Bolognese",
+		"A heart warming classic, perfect for a cold winter's night",
+		"JDEEsQKEm7I",
+		[]Ingredient{
+			{2, "tbs", "Olive oil"},
+			{2, "", "Brown onion"},
+			{4, "cloves", "Garlic"},
+			{1, "kg", "Beef mince"},
+			{90, "g", "Tomato paste"},
+			{500, "ml", "Beef stock"},
+			{2, "cans", "Chopped tomatoes"},
+			{1, "", "Bay leaf"},
+			{400, "g", "Spaghetti"},
+			{50, "g", "Shaved Parmesan"},
+		},
+		[]Step{
+			{"Roughly chop brown onions"},
+			{"Skin and finely mince garlic"},
+		},
+		[]Step{
+			{"Heat oil in a pan, once shimmering add onion and stir over medium heat until softened"},
+			{"Add garlic and cook for 20 seconds, until fragrant"},
+			{"Add Beef mince and cook for 5 minutes, breaking apart and turning until browned all over"},
+			{"Add tomatoes, tomato paste, beef stock and season with salt and pepper. Bring to a boild, then simmer for an hour until reduced"},
+			{"Cook pasta in salted water until al dente"},
+			{"Reserve a couple of cups of the pasta water, then drain the pasta"},
+			{"In a frypan combine the pasta, a healthy serving of sauce, and some pasta water. Agitate over heat until combined"},
+		},
+	},
+}
 
 func main() {
 
@@ -42,15 +143,37 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
+	json.NewEncoder(w).Encode("Flutter Recipe API")
 }
 
 func recipesListHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{"message": "recipes!"})
+	var recipeHeaders []RecipeHeader
+	for _, recipe := range recipes {
+		recipeHeaders = append(recipeHeaders, RecipeHeader{
+			recipe.ID,
+			recipe.Name,
+			recipe.ShortDescription,
+			recipe.ImageID,
+		})
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(recipeHeaders)
 }
 
 func recipeHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	id := vars["id"]
-	json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprintf("Recipe %v", id)})
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Printf("Couldn't convert id '%v' to int: %v", vars["id"], err)
+		http.NotFound(w, r)
+		return
+	}
+	for _, recipe := range recipes {
+		if recipe.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(recipe)
+			return
+		}
+	}
+	http.NotFound(w, r)
 }
